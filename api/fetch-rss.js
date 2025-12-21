1601fa08f1055e49271e1c3e086b9dcd4cfd2be0
lellivalentina1992-lang <lelli.valentina1992@gmail.com>
2025-12-21 13:21:01 +0100

    Create fetch-rss.js

---
+++ api/fetch-rss.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const { url } = req.query;
+  if (!url) return res.status(400).json({ error: "URL mancante" });
+  try {
+    const response = await fetch(url);
+    const xml = await response.text();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/xml');
+    res.status(200).send(xml);
+  } catch (error) {
+    res.status(500).json({ error: "Errore caricamento feed" });
+  }
+}  
